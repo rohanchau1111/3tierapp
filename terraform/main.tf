bd01cf353d3ae3c6ff54bf2cff332bfaa164cb25
rohanchau1111 <chaudharirohan938@gmail.com>
2026-02-03 09:51:00 +0530

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ module "aks" {
   location            = var.location
   aks_name            = var.aks_name
   subnet_id           = module.vnet.subnet_id
-}
+} 
 
 resource "azurerm_role_assignment" "aks_acr_pull" {
   principal_id         = module.aks.kubelet_object_id
